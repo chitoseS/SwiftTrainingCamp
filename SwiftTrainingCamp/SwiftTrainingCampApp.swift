d73b98ea2ccafd1f4cd8e7ae0a9ebc9903ce4201
m_kimura <chitosws.apple@gmail.com>
2024-06-15 20:18:38 +0900

    Initial Commit

---
+++ SwiftTrainingCamp/SwiftTrainingCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTrainingCampApp.swift
+//  SwiftTrainingCamp
+//
+//  Created by Motoki Kimura on 2024/06/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTrainingCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
